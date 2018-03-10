689355ba44986b171ae00af202e535b27383bbf8
Vadim Kravtsov <mr.vadim.kravtsov@mail.ru>
2018-03-10 21:41:14 +0300

    Change README.md

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 Meteostation on Arduino with **DHT22** **humidity** and **temperature** sensor, **GY-2561** **flux** sensor and **MLX90615** **infrared temperature** sensor.:
 ## Scheme
 ![scheme.png](https://github.com/vadim-kravtsov/meteoStation/blob/master/scheme.png)
+## Realization
+![sideview.png](https://github.com/vadim-kravtsov/meteoStation/blob/master/sideview.png)
+![topview.png](https://github.com/vadim-kravtsov/meteoStation/blob/master/topview.png)
+## Data
+![result.png](https://github.com/vadim-kravtsov/meteoStation/blob/master/result.png)
--- plotter.py
+++ plotter.py
@@ -4,8 +4,8 @@ import matplotlib.animation as animation
 from matplotlib import style
 
 style.use('bmh')
-fig, axes = plt.subplots(nrows=3, ncols=1)
-ax1, ax2, ax4 = axes.flatten()
+fig, axes = plt.subplots(nrows=2, ncols=2)
+ax1, ax2, ax3, ax4 = axes.flatten()
 
 def animate(i):
 	data = open('data.txt', 'r').read()
@@ -28,20 +28,20 @@ def animate(i):
 			humidity.append(float(h))
 	ax1.clear()
 	ax2.clear()
-	#ax3.clear()
+	ax3.clear()
 	ax4.clear()
 	ax1.plot(times, skyTemp, 'C0')
 	ax2.plot(times, humidity, 'C1')
-	#ax3.plot(time, flux, 'navy')
+	ax3.plot(times, flux, 'navy')
 	ax4.plot(times, dhtTemp, 'C3')
 	ax4.plot(times, ambTemp, 'C4')
 	ax1.grid(True)
 	ax2.grid(True)
-	#ax3.grid(True)
+	ax3.grid(True)
 	ax4.grid(True)
-	ax1.set_title('Temperature, $^\circ$C')
-	ax2.set_title('Humidity, %')
-	#ax3.set_title('Flux')
+	ax1.set_title('Sky temperature, $^\circ$C')
+	ax2.set_title('Humidity, \%')
+	ax3.set_title('Flux, lux')
 	ax4.set_title('Temperature, $^\circ$C')
 	plt.gcf().autofmt_xdate()
 
